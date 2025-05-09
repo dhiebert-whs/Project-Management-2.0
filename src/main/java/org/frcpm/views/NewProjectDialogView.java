7733fd7e98058e1a645187ac270bb7c4f030f1f0
dhiebert-whs <dhiebert@hazelwoodschools.org>
2025-05-09 08:43:39 -0500

    .

--- src/main/java/org/frcpm/views/NewProjectDialogView.java
+++ src/main/java/org/frcpm/views/NewProjectDialogView.java
@@ -1,12 +1,26 @@
 package org.frcpm.views;
 
 import com.airhacks.afterburner.views.FXMLView;
+import java.util.ResourceBundle;
 
 /**
  * View class for the new project dialog following AfterburnerFX convention.
  */
 public class NewProjectDialogView extends FXMLView {
-    // By convention, the corresponding FXML file should be at 
-    // src/main/resources/org/frcpm/views/newprojectdialog.fxml (without the "view" suffix)
-    // And the controller should be NewProjectDialogPresenter.java in the presenters package
+    
+    /**
+     * Default constructor.
+     */
+    public NewProjectDialogView() {
+        super();
+    }
+    
+    /**
+     * Constructor with resource bundle.
+     * 
+     * @param resources the resource bundle
+     */
+    public NewProjectDialogView(ResourceBundle resources) {
+        super(resources);
+    }
 }
